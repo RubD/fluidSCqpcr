6393b69302219ecdc093395935edf26862348583
rubendries <rubendries@gmail.com>
2015-08-17 19:51:42 +0200

    fixed errors

--- R/dotplotSC_function.R
+++ R/dotplotSC_function.R
@@ -16,9 +16,10 @@
 #' @examples
 #' dotplotSC()
 
-dotplotSC <- function (fluidSCproc,  based_on_values = "log2Ex", assayCol = "Assays", assays = c("Rpl13a", "Pou5f1"),
-                       missingCol = NULL, facetCol = NULL, coord_ratio = 0.6, binwidth = 0.6, ylimit = 25) 
-{
+dotplotSC <- function(fluidSCproc,  based_on_values = "log2Ex", assayCol = "Assays", assays = c("Rpl13a", "Pou5f1"),
+                      missingCol = NULL, facetCol = NULL, coord_ratio = 0.6, binwidth = 0.6, ylimit = 25) {
+  
+  if(nargs() == 0) stop(paste0("you need to provide parameters, for more info see ?",sys.call()))
   
   normFluidCt <- fluidSCproc$data
   normFluidCt <- normFluidCt[normFluidCt[ ,assayCol] %in% assays,]
@@ -29,58 +30,58 @@ dotplotSC <- function (fluidSCproc,  based_on_values = "log2Ex", assayCol = "Ass
   if(is.null(facetCol)) {
     
     if (!is.null(missingCol)) {
-      plot <- ggplot(normFluidCt, aes_string(x = based_on_values, fill = missingCol))
-      plot <- plot + geom_dotplot(binwidth = binwidth)
-      plot <- plot + ylim(0, ylimit) + xlim(0, max_x_limit)
-      plot <- plot + coord_fixed(ratio = coord_ratio)
-      plot <- plot + facet_wrap(as.formula(paste("~", assayCol)))
-      plot <- plot + theme_bw()
-      plot <- plot + theme(axis.title = element_text(size = 22))
-      plot <- plot + labs(x = based_on_values, y = "number of cells")
+      myplot <- ggplot(normFluidCt, aes_string(x = based_on_values, fill = missingCol))
+      myplot <- myplot + geom_dotplot(binwidth = binwidth)
+      myplot <- myplot + ylim(0, ylimit) + xlim(0, max_x_limit)
+      myplot <- myplot + coord_fixed(ratio = coord_ratio)
+      myplot <- myplot + facet_wrap(as.formula(paste("~", assayCol)))
+      myplot <- myplot + theme_bw()
+      myplot <- myplot + theme(axis.title = element_text(size = 22))
+      myplot <- myplot + labs(x = based_on_values, y = "number of cells")
     }
     else if (is.null(missingCol)) {
-      plot <- ggplot(normFluidCt, aes_string(x = based_on_values))
-      plot <- plot + geom_dotplot(binwidth = binwidth)
-      plot <- plot + scale_fill_identity(guide = guide_legend(title = "Expression"))
-      plot <- plot + ylim(0, ylimit) + xlim(0, max_x_limit)
-      plot <- plot + coord_fixed(ratio = coord_ratio)
-      plot <- plot + facet_wrap(as.formula(paste("~", assayCol)))
-      plot <- plot + theme_bw()
-      plot <- plot + theme(axis.title = element_text(size = 22))
-      plot <- plot + labs(x = based_on_values, y = "number of cells")
-    }
-    
-    else if (!is.null(facetCol)) {
-      
-      if (!is.null(missingCol)) {
-        plot <- ggplot(normFluidCt, aes_string(x = based_on_values, fill = missingCol))
-        plot <- plot + geom_dotplot(binwidth = binwidth)
-        plot <- plot + ylim(0, ylimit) + xlim(0, max_x_limit)
-        plot <- plot + coord_fixed(ratio = coord_ratio)
-        plot <- plot + facet_grid(as.formula(paste(facetCol,"~", assayCol)))
-        plot <- plot + theme_bw()
-        plot <- plot + theme(axis.title = element_text(size = 22))
-        plot <- plot + labs(x = based_on_values, y = "number of cells")
-      }
-      else if (is.null(missingCol)) {
-        plot <- ggplot(normFluidCt, aes_string(x = based_on_values))
-        plot <- plot + geom_dotplot(binwidth = binwidth)
-        plot <- plot + scale_fill_identity(guide = guide_legend(title = "Expression"))
-        plot <- plot + ylim(0, ylimit) + xlim(0, max_x_limit)
-        plot <- plot + coord_fixed(ratio = coord_ratio)
-        plot <- plot + facet_grid(as.formula(paste(facetCol,"~", assayCol)))
-        plot <- plot + theme_bw()
-        plot <- plot + theme(axis.title = element_text(size = 22))
-        plot <- plot + labs(x = based_on_values, y = "number of cells")
-      }
-      
-      
+      myplot <- ggplot(normFluidCt, aes_string(x = based_on_values))
+      myplot <- myplot + geom_dotplot(binwidth = binwidth)
+      myplot <- myplot + scale_fill_identity(guide = guide_legend(title = "Expression"))
+      myplot <- myplot + ylim(0, ylimit) + xlim(0, max_x_limit)
+      myplot <- myplot + coord_fixed(ratio = coord_ratio)
+      myplot <- myplot + facet_wrap(as.formula(paste("~", assayCol)))
+      myplot <- myplot + theme_bw()
+      myplot <- myplot + theme(axis.title = element_text(size = 22))
+      myplot <- myplot + labs(x = based_on_values, y = "number of cells")
     }
     
   }
   
+  else if (!is.null(facetCol)) {
+    
+    if (!is.null(missingCol)) {
+      myplot <- ggplot(normFluidCt, aes_string(x = based_on_values, fill = missingCol))
+      myplot <- myplot + geom_dotplot(binwidth = binwidth)
+      myplot <- myplot + ylim(0, ylimit) + xlim(0, max_x_limit)
+      myplot <- myplot + coord_fixed(ratio = coord_ratio)
+      myplot <- myplot + facet_grid(as.formula(paste(facetCol,"~", assayCol)))
+      myplot <- myplot + theme_bw()
+      myplot <- myplot + theme(axis.title = element_text(size = 22))
+      myplot <- myplot + labs(x = based_on_values, y = "number of cells")
+    }
+    else if (is.null(missingCol)) {
+      myplot <- ggplot(normFluidCt, aes_string(x = based_on_values))
+      myplot <- myplot + geom_dotplot(binwidth = binwidth)
+      myplot <- myplot + scale_fill_identity(guide = guide_legend(title = "Expression"))
+      myplot <- myplot + ylim(0, ylimit) + xlim(0, max_x_limit)
+      myplot <- myplot + coord_fixed(ratio = coord_ratio)
+      myplot <- myplot + facet_grid(as.formula(paste(facetCol,"~", assayCol)))
+      myplot <- myplot + theme_bw()
+      myplot <- myplot + theme(axis.title = element_text(size = 22))
+      myplot <- myplot + labs(x = based_on_values, y = "number of cells")
+    }
+    
+  }
   
-  print(plot)
+  print(myplot)
   
 }
+
+
 
